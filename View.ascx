810e4bdfa6bf47ccb40da73f71fe0a2cce842ccb
birksy89 <andy.birks@gmail.com>
2014-10-13 17:39:36 +0100

    Multiple Images

--- View.ascx
+++ View.ascx
@@ -1,8 +1,15 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="Christoc.Modules.SQLWorldDBv1.View" %>
 
+<asp:Repeater ID="Repeater1" runat="server">
+
+</asp:Repeater>
+<hr />
 
 <asp:Button ID="btnName" runat="server" Text="Name" OnClick="btnName_Click" />
 <asp:Button ID="btnArea" runat="server" Text="Surface Area" OnClick="btnArea_Click" />
+
+<hr />
+
 <asp:Literal ID="Literal1" runat="server"></asp:Literal>
 
 <div id="RazorDiv1" runat="server"></div>
--- View.ascx.designer.cs
+++ View.ascx.designer.cs
@@ -13,6 +13,15 @@ namespace Christoc.Modules.SQLWorldDBv1 {
     public partial class View {
         
         /// <summary>
+        /// Repeater1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+        
+        /// <summary>
         /// btnName control.
         /// </summary>
         /// <remarks>
